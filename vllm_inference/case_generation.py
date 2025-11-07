19c91330b2f96ad4d723242e2a2eff8c589cb2d1
zjhox <zjhooxx00@163.com>
2025-11-07 23:13:38 +0800

    fix

--- vllm_inference/case_generation.py
+++ vllm_inference/case_generation.py
@@ -1,34 +1,120 @@
 
-info_dict_demo = {
+field_dict_demo = {
     'age': {
         'indicator_name': 'Age at recruitment',
         'indicator_name_cn': '年龄',
         'units': 'years',
         'units_cn': '岁',
-        'category': 'basic information',
-        'category_cn': '基本信息',
         'comment': '',
     },
 }
 
-category_list = [
+category_list  = [
     {
-        'category_cn': '基本信息',
-        'category': 'basic information',
-        'field_dict': {
-            'age': {
+        'id': '1',
+        'name_cn': '基本信息',
+        'name': 'basic information',
+        'field_list': [],
+    },
+    {
+        'id': '2',
+        'name_cn': '社会经济因素',
+        'name': 'socioeconomic factors',
+        'field_list': [],
+    },
+    {
+        'id': '3',
+        'name_cn': '健康因素',
+        'name': 'health factors',
+        'field_list': [],
+    },
+]
+
+units_dict = {
+    '1': ('no units', '无单位'),
+    '100001': ('years', '岁'),
+    '100002': ('months', '月'),
+    '100003': ('days', '天'),
+}
+
+
+def get_field_dict(dict_file_path):
+    """
+    从 dict_file_path 中获取指标信息字典; csv or xlsx 格式如下：
+    field_id,indicator_name,indicator_name_cn,field_name,units_id,category_id
+
+    构建 field_dict 和 category_list，格式如下：
+
+    field_dict = {
+        'age': {
+            'field_id': '1',
             'indicator_name': 'Age at recruitment',
             'indicator_name_cn': '年龄',
             'units': 'years',
             'units_cn': '岁',
             'comment': '',
-            }
+        },
+    }
+
+    category_list  = [
+        {
+            'id': '1',
+            'name_cn': '基本信息',
+            'name': 'basic information',
+            'field_list': ['age', ......],
+        },
+        ......
+    ]
+
+    """
+    import pandas as pd
+    
+    if dict_file_path.endswith('.csv'):
+        df = pd.read_csv(dict_file_path)
+    elif dict_file_path.endswith('.xlsx'):
+        df = pd.read_excel(dict_file_path, sheet_name='dict')
+    else:
+        raise ValueError('dict_file_path must be csv or xlsx file')
+    
+    field_dict = {}
+    for _, row in df.iterrows():
+        field_id,indicator_name,indicator_name_cn,field_name,units_id,category_id = row
+        field_dict[field_name] = {
+            'field_id': field_id,
+            'indicator_name': indicator_name,
+            'indicator_name_cn': indicator_name_cn,
+            'units': units_dict[units_id][0],
+            'units_cn': units_dict[units_id][1],
+            'comment': '',
         }
-    },
-]
+        for category in category_list:
+            if category['id'] == category_id:
+                category['field_list'].append(field_name)
+                break
+    return field_dict, category_list
 
+def generate_case_json_list(csv_path, category_list, field_dict,cn=False):
+    """
+    从 csv_path 中读取数据，根据 category_list 和 field_dict 生成样本, 每个样本为一个 json 字符串列表，格式如下：
+    {"eid": 1, "input": "[Basic information]\nAge: 56 years old\nGender: female\n......"]}
 
-def generate_case(row, info_dict, cn=False):
-    if cn:
-        for field in info_dict:
-            field_context = field['indicator_name_cn'] + ':' + row[field] + ' ' + field['units_cn']
+    """
+    case_josnl = []
+    import pandas as pd
+    df = pd.read_csv(csv_path)
+    for _, row in df.iterrows():
+        eid = row['eid']
+        input = ''
+        for category in category_list:
+            category_name = category['name_cn'] if cn else category['name']
+            input += f'[{category_name}]\n'
+            for field_name in category['field_list']:
+                field_info = field_dict[field_name]
+                indicator_name = field_info['indicator_name_cn'] if cn else field_info['indicator_name']
+                units = field_info['units_cn'] if cn else field_info['units']
+                input += f'{indicator_name}: {row[field_name]} {units}\n'
+        case_josnl.append({
+            'eid': eid,
+            'input': input,
+        })
+    return case_josnl
---
+++ vllm_inference/csv2context_map_dict.py
@@ -0,0 +1,62 @@
+import pandas as pd
+
+def generate_category_list(category_df: pd.DataFrame):
+    """
+    从 category_df 中读取数据，生成 category_list，格式如下：
+    category_list  = [
+        {
+            'id': '1',
+            'name_cn': '基本信息',
+            'name': 'basic information',
+            'field_list': [],
+        },
+        {
+            'id': '2',
+            'name_cn': '社会经济因素',
+            'name': 'socioeconomic factors',
+            'field_list': [],
+        },
+        {
+            'id': '3',
+            'name_cn': '健康因素',
+            'name': 'health factors',
+            'field_list': [],
+        },
+    ]
+    """
+    category_list = []
+    for _, row in category_df.iterrows():
+        category_list.append({
+            'id': row['category_index'],
+            'name_cn': row['category_cn'],
+            'name': row['category'],
+            'field_list': [],
+        })
+    return category_list
+
+
+def generate_units_dict(units_df: pd.DataFrame):
+    """
+    从 units_df 中读取数据，生成 units_dict，格式如下：
+    units_dict = {
+        '1': ('', ''),
+        '100001': ('years', '岁'),
+        '100002': ('months', '月'),
+        '100003': ('days', '天'),
+    }
+    """
+    units_dict = {}
+    for _, row in units_df.iterrows():
+        # todo: 中文单位待补充
+        units_dict[row['unit_index']] = (row['unit_name'], row['unit_name'])
+    return units_dict
+
+
+def generate_csv2context_map_dict(xlsx_path):
+    """
+    从 xlsx_path 中读取数据，生成指标到上下文的映射字典或列表，包括category_list,units_dict
+    """
+    df = pd.read_excel(xlsx_path, sheet_name=['category', 'units'])
+    category_list  = generate_category_list(df['category'])
+    units_dict = generate_units_dict(df['units'])
+    return category_list, units_dict
